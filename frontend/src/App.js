ce0fb59994aa09db6c280fac540c9e7e79291bf3
hrshlmeht <harshalmehta1998@gmail.com>
2022-10-19 00:11:16 -0400

    25.38

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navigation from './components/shared/Navigation/Navigation';
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
+import Authenticate from './pages/authenticate/Authenticate';
+import React, { useState } from 'react';
+
+const isAuth = true;
+
 function App() {
     return (
         <BrowserRouter>
@@ -12,15 +17,36 @@ function App() {
                 <Route path="/" exact>
                     <Home />
                 </Route>
-                <Route path="/register">
-                    <Register />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
+                <GuestRoute path="/authenticate">
+                    <Authenticate />
+                </GuestRoute>
             </Switch>
         </BrowserRouter>
     );
+}
+
+
+
+const GuestRoute = ({children ,...rest}) => {
+
+    return(
+        <Route {...rest}
+       return  render={(location)=>{
+           return  isAuth ?
+            <Redirect to = {
+                {
+                    pathname : '/rooms',
+                    state :{from:'location'}
+                }}
+            />
+            : (
+                children 
+              )
+        }}>
+
+        </Route>
+    );
+
 }
 
 export default App;
