16d1b2f1a61499135108c8e104f50b97fe70e8cd
hrshlmeht <harshalmehta1998@gmail.com>
2022-11-02 23:47:59 -0400

    OTP is generated

--- backend/controllers/auth-controllers.js
+++ backend/controllers/auth-controllers.js
@@ -1,27 +1,72 @@
 const otpService = require('../services/otp-service')
 const hashService = require('../services/hash-service')
 
+
+
+
 class AuthController{
 
-     async sendOtp(req,res)
-    {
-        const {phone} = req.body;
-        if (!phone){
-            res.status(400).json({message : 'Phone field is required'})
+    //  async sendOtp(req,res)
+    // {
+    //     const {phone} = req.body;
+    //     if (!phone){
+    //         res.status(400).json({message : 'Phone field is required'})
 
+    //     }
+    //     const otp =  await otpService.generateOtp();
+
+
+    //     const ttl = 1000 * 60 * 3; // 3 min
+    //     const expires = Date.now() + ttl;
+    //     const data = `${phone}.${otp}.${expires}`;
+    //     const hash = hashService.hashOtp(data);
+
+
+    //     try{
+    //     await otpService.sendBySms(phone ,otp)
+    //     res.json({
+    //         hash : `${hash}.${expires}`,
+    //         phone,
+    //     })
+    //     console.log("TRY")
+
+    //     }
+
+    //     catch(err){
+    //         console.log(err)
+    //         res.status(500).json({message: 'Message sending failed'})
+    //         console.log("CATHC")
+    //     }
+       
+    //     res.json({hash: hash });
+
+    // }
+
+    async sendOtp(req, res) {
+        const { phone } = req.body;
+        if (!phone) {
+            res.status(400).json({ message: 'Phone field is required!' });
         }
-        const otp =  await otpService.generateOtp();
 
+        const otp = await otpService.generateOtp();
 
         const ttl = 1000 * 60 * 2; // 2 min
         const expires = Date.now() + ttl;
         const data = `${phone}.${otp}.${expires}`;
         const hash = hashService.hashOtp(data);
 
-
-       
-        res.json({hash: hash });
-
+        // send OTP
+        try {
+            await otpService.sendBySms(phone, otp);
+            res.json({
+                hash: `${hash}.${expires}`,
+                phone,
+                otp,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: 'message sending failed' });
+        }
     }
 }
 
--- backend/services/otp-service.js
+++ backend/services/otp-service.js
@@ -1,9 +1,23 @@
 const crypto = require('crypto');
 
+const smsSid = process.env.SMS_SID
+const smsAuthToken = process.env.SMS_AUTH_TOKEN
+const twilio = require('twilio')(smsSid , smsAuthToken , {
+    lazyLoading : true
+})
+
 class OtpService {
      async generateOtp() {
         const otp = crypto.randomInt(1000, 9999);
         return otp;
+    }
+
+    async sendBySms(phone , otp){
+        return twilio.messages.create({
+            to: phone,
+            from : process.env.SMS_FROM_NUMBER,
+            body : `Your OTP is ${otp}`,
+        })
     }
 
    
